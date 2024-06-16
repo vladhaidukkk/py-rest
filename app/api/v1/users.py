9dea89d6c2d38f3b2a62340e7ef1d0681cbcb2f8
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-06-16 14:21:36 +0300

    create endpoint to get users

--- app/api/v1/users.py
+++ app/api/v1/users.py
@@ -1,3 +1,18 @@
-from fastapi import APIRouter
+from typing import Annotated, Sequence
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.db import db_helper
+from app.core.db.models import User
+from app.core.schemas.user import UserRead
+from app.crud.users import get_all_users
 
 router = APIRouter(tags=["Users"])
+
+
+@router.get("", response_model=list[UserRead])
+async def get_users(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+) -> Sequence[User]:
+    return await get_all_users(session)
