45a5a3563e6be9221b220e0480f8dff0d1cf3155
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-06-16 14:34:07 +0300

    create endpoint to create user

--- app/api/v1/users.py
+++ app/api/v1/users.py
@@ -5,8 +5,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db import db_helper
 from app.core.db.models import User
-from app.core.schemas.user import UserRead
-from app.crud.users import get_all_users
+from app.core.schemas.user import UserCreate, UserRead
+from app.crud import users as users_crud
 
 router = APIRouter(tags=["Users"])
 
@@ -15,4 +15,12 @@ router = APIRouter(tags=["Users"])
 async def get_users(
     session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
 ) -> Sequence[User]:
-    return await get_all_users(session)
+    return await users_crud.get_all_users(session=session)
+
+
+@router.post("", response_model=UserRead)
+async def create_user(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    user_create: UserCreate,
+) -> User:
+    return await users_crud.create_user(session=session, user_create=user_create)
--- app/crud/users.py
+++ app/crud/users.py
@@ -4,9 +4,20 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db.models import User
+from app.core.schemas.user import UserCreate
 
 
 async def get_all_users(session: AsyncSession) -> Sequence[User]:
     query = select(User).order_by(User.id)
     result = await session.scalars(query)
     return result.all()
+
+
+async def create_user(session: AsyncSession, user_create: UserCreate) -> User:
+    user = User(
+        username=user_create.username,
+        password=user_create.password.get_secret_value(),
+    )
+    session.add(user)
+    await session.commit()
+    return user
