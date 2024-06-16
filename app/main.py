e14b8272e3c2f44973602e8fc76f7d02e09aa9e6
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-06-16 09:01:45 +0300

    create package for api interface

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
 
+from app.api import router as api_router
+
 app = FastAPI()
+app.include_router(api_router, prefix="/api")
 
 if __name__ == "__main__":
     uvicorn.run("app.main:app", reload=True)
