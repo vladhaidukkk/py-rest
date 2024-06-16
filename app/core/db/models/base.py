5e16b2ed923305eb4342e0c24484d7ddec406d76
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-06-16 13:07:37 +0300

    improve naming conventions for db constraints

--- app/core/db/models/base.py
+++ app/core/db/models/base.py
@@ -3,13 +3,10 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 metadata = MetaData(
     naming_convention={
-        "all_column_names": lambda constraint, _table: "_".join(
-            [column.name for column in constraint.columns.values()]  # type: ignore[attr-defined]
-        ),
-        "ix": "ix_%(table_name)s_%(all_column_names)s",
-        "uq": "uq_%(table_name)s_%(all_column_names)s",
+        "ix": "ix_%(table_name)s_%(column_0_N_name)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
         "ck": "ck_%(table_name)s_%(constraint_name)s",
-        "fk": "fk_%(table_name)s_%(all_column_names)s_%(referred_table_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_N_name)s_%(referred_table_name)s",
         "pk": "pk_%(table_name)s",
     }
 )
